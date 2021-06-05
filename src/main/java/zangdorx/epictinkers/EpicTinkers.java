9f3f12fb4dc4bf78d5eb3fb2dfdc7034364bf625
philippewarren <philippewarren31@gmail.com>
2021-06-04 21:18:58 -0400

    Compiles now

--- src/main/java/zangdorx/epictinkers/EpicTinkers.java
+++ src/main/java/zangdorx/epictinkers/EpicTinkers.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package zangdorx.epictinkers;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("epictinkers")
+public class EpicTinkers
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public EpicTinkers() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
